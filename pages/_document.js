375a27452324e08b6522105d03234ebdae3c2409
François Côté <francois@francoiscote.net>
2022-01-31 16:05:34 -0500

    fix crossOrigin attribute

--- pages/_document.js
+++ pages/_document.js
@@ -12,7 +12,11 @@ class CustomDocument extends Document {
         <Head>
           {/* Google Fonts */}
           <link rel="preconnect" href="https://fonts.googleapis.com" />
-          <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
+          <link
+            rel="preconnect"
+            href="https://fonts.gstatic.com"
+            crossOrigin="true"
+          />
           <link
             href="https://fonts.googleapis.com/css2?family=DM+Sans:wght@400;500;700&display=swap"
             rel="stylesheet"
