be3c9654ee53d8cd8f2e642248c8d231c180ead8
François Côté <francois@francoiscote.net>
2023-08-11 11:06:52 -0400

    rss feed

--- .eleventy.js
+++ .eleventy.js
@@ -1,16 +1,19 @@
+// Plugins
 const eleventySass = require("eleventy-sass");
+const pluginRss = require("@11ty/eleventy-plugin-rss");
 
 // Filters
 const dateFilter = require("./src/filters/date-filter.js");
 const w3DateFilter = require("./src/filters/w3-date-filter.js");
 
 module.exports = (config) => {
+  // Add Plugins
+  config.addPlugin(eleventySass);
+  config.addPlugin(pluginRss);
+
   // Add Filters
   config.addFilter("dateFilter", dateFilter);
   config.addFilter("w3DateFilter", w3DateFilter);
-
-  // Add Plugins
-  config.addPlugin(eleventySass);
 
   config.addPassthroughCopy("images");
 
